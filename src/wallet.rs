4bbffda8677f4ba243c01f3b8c03d4ed89a56669
Chris Czub <chris@penumbra.zone>
2022-06-06 21:28:21 -0400

    Don't bail on change check

--- src/wallet.rs
+++ src/wallet.rs
@@ -258,8 +258,10 @@ impl<V: ViewClient, C: CustodyClient> WalletWorker<V, C> {
                     // TODO: remove??
                     // self.sync_one_block().await?;
                 } else {
-                    tracing::warn!("not enough funds to complete transaction");
-                    anyhow::bail!("not enough funds to complete transaction");
+                    // TODO: this was being used for checking that change was returned from the transaction
+                    tracing::debug!("unimplemented: change checking");
+                    // tracing::warn!("not enough funds to complete transaction");
+                    // anyhow::bail!("not enough funds to complete transaction");
                 }
             } else {
                 // If ready, proceed
