563284cd4f32948042a30bf354e0c580dfae0313
katelyn martin <katelyn@penumbralabs.xyz>
2024-06-04 00:00:00 +0000

    proto: 🤝 add compatibility layer between penumbra protos

--- src/proto.rs
+++ src/proto.rs
@@ -9,6 +9,9 @@ mod galileo {
     }
 }
 
+/// Vendored Penumbra protobuf types.
+///
+/// See [`compat`][self::compat] for more information.
 pub mod penumbra {
     pub mod core {
         pub mod keys {
@@ -20,6 +23,52 @@ pub mod penumbra {
             pub mod v1 {
                 include!("proto/gen/penumbra.core.txhash.v1.rs");
             }
+        }
+    }
+}
+
+/// Compatibility facilities.
+///
+/// This provides glue to connect our vendored protobuf definitions to the canonical
+/// [`penumbra_proto`] definitions.
+mod compat {
+    // === impl Address ===
+
+    type DomainAddress = penumbra_keys::Address;
+    type CanonicalAddress = penumbra_proto::core::keys::v1::Address;
+    type VendoredAddress = super::penumbra::core::keys::v1::Address;
+
+    impl Into<CanonicalAddress> for VendoredAddress {
+        fn into(self) -> CanonicalAddress {
+            let Self { inner, alt_bech32m } = self;
+            CanonicalAddress { inner, alt_bech32m }
+        }
+    }
+
+    impl TryInto<DomainAddress> for VendoredAddress {
+        type Error = <DomainAddress as TryFrom<CanonicalAddress>>::Error;
+        fn try_into(self) -> Result<DomainAddress, Self::Error> {
+            let address: CanonicalAddress = self.into();
+            DomainAddress::try_from(address)
+        }
+    }
+
+    // === impl TransactionId ===
+
+    type DomainTransactionId = penumbra_txhash::TransactionId;
+    type CanonicalTransactionId = penumbra_proto::core::txhash::v1::TransactionId;
+    type VendoredTransactionId = super::penumbra::core::txhash::v1::TransactionId;
+
+    impl From<CanonicalTransactionId> for VendoredTransactionId {
+        fn from(CanonicalTransactionId { inner }: CanonicalTransactionId) -> Self {
+            Self { inner }
+        }
+    }
+
+    impl Into<VendoredTransactionId> for DomainTransactionId {
+        fn into(self) -> VendoredTransactionId {
+            let id: CanonicalTransactionId = self.into();
+            id.into()
         }
     }
 }
